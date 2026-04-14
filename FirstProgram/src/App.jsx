3a3095075c8834ed6bd697679690b93ebf926977
MOHD ABDULLAH AZAM <2400101530075@aktu.edu.in>
2026-04-14 07:34:45 +0000

    done

---
+++ FirstProgram/src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [data, setCount] = useState("hello")
+  const [isOn, setIsOn] = useState(false);
+
+  function toggleButton()
+  {
+    setIsOn(!isOn)
+  }
+  function fun()
+  {
+    setCount("bye")
+  }
+
+  return (
+    <>
+      <h1>State of variable</h1>
+      <h1>{data}</h1>
+      <button onClick={fun}>Change Text</button>
+      <h1>Toggle Button</h1>
+      <h3>Status: {isOn ? "ON" : "OFF"}</h3>
+      <button onClick={toggleButton}>{isOn ? "Turn OFF" : "Turn ON"}</button>
+    </>
+  )
+}
+
+export default App;
